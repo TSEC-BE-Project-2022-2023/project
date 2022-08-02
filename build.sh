9eb141cf9edede0019d3e725e165d243a59f575f
Kartik Soneji <kartiksoneji@rocketmail.com>
2022-08-02 20:24:16 +0530

    Initial commit

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -eu;
+
+output="${1-./build}";
+echo "Building to: '${output}'";
+
+mkdir -p "${output}";
+
+# Workaround for running as root
+# https://github.com/mermaid-js/mermaid-cli/blob/master/docs/linux-sandbox-issue.md
+
+PUPPETEER_CONFIG="";
+if [ "$(id -u)" -eq 0 ] && [ -n "${CI}" ]; then
+	PUPPETEER_CONFIG="puppeteer-config.json";
+	echo '{ "args": ["--no-sandbox"] }' > "${PUPPETEER_CONFIG}";
+	PUPPETEER_CONFIG=$(realpath "${PUPPETEER_CONFIG}");
+fi
+
+asciidoctor-revealjs -vw -r asciidoctor-diagram \
+	${PUPPETEER_CONFIG+-a "mermaid-puppeteer-config=${PUPPETEER_CONFIG}"} \
+	"presentations/01 Topic Approval - Synthesizer.adoc" \
+	-o "${output}/presentations/01 Topic Approval - Synthesizer.html";
+
+asciidoctor-revealjs -vw -r asciidoctor-diagram \
+	${PUPPETEER_CONFIG+-a "mermaid-puppeteer-config=${PUPPETEER_CONFIG}"} \
+	"presentations/01 Topic Approval - GUI.adoc" \
+	-o "${output}/presentations/01 Topic Approval - GUI.html";
+
+asciidoctor -vw "README.adoc" -o "${output}/index.html";
+
+echo "Build complete";
