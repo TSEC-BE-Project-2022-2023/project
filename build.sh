e542728a5554e671f31e0450821f411286b49d4f
Kartik Soneji <kartiksoneji@rocketmail.com>
2022-08-03 20:13:37 +0530

    Fix static assets not present in build folder

--- build.sh
+++ build.sh
@@ -17,11 +17,15 @@ if [ "$(id -u)" -eq 0 ] && [ -n "${CI}" ]; then
 	PUPPETEER_CONFIG=$(realpath "${PUPPETEER_CONFIG}");
 fi
 
+# Build presentations
+cp -r \
+	"presentations/images" \
+	"presentations/style.css" \
+	"${output}/presentations";
 asciidoctor-revealjs -vw -r asciidoctor-diagram \
 	${PUPPETEER_CONFIG+-a "mermaid-puppeteer-config=${PUPPETEER_CONFIG}"} \
 	"presentations/01 Topic Approval - Synthesizer.adoc" \
 	-o "${output}/presentations/01 Topic Approval - Synthesizer.html";
-
 asciidoctor-revealjs -vw -r asciidoctor-diagram \
 	${PUPPETEER_CONFIG+-a "mermaid-puppeteer-config=${PUPPETEER_CONFIG}"} \
 	"presentations/01 Topic Approval - GUI.adoc" \
